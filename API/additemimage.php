ea6f0ae4090e744b9231d8f630873b9a4d016a09
Fahreza <fahreza_12@apps.ipb.ac.id>
2019-05-06 02:02:27 +0700

    update additemimage.php
    image is now a column in item table. one item can only have one image.

--- API/additemimage.php
+++ API/additemimage.php
@@ -9,6 +9,6 @@ $imagePath = "./images/".$image;
 
 move_uploaded_file($_FILES['image']['tmp_name'], $imagePath);
 
-$db->query("INSERT INTO item_image (user_id, item_name, image_file) VALUES('$user_id','$item_name', '$image')");
+$db->query("UPDATE item SET image = '$image' WHERE user_id = '$user_id' AND name = '$item_name'");
 
 ?>
